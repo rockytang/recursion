853e87e3dcbf2972c5cbde28d83dbebd991e4def
Rocky Tang <rocky.tang168@gmail.com>
2015-03-05 15:53:17 -0800

    solved half the tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,45 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var str = "";
+
+  if(typeof obj === "number"){
+  	str = str + obj;
+  }
+
+  if(typeof obj === "boolean"){
+  	str = str + obj;
+  }
+  if(typeof obj === "string"){
+  	str = '"' + str + obj + '"';
+  }
+  if(Array.isArray(obj)){
+  	
+  	if(obj.length > 1) {
+	  	str = str + "[";
+
+	  	for(var i = 0; i < obj.length; i++) {
+
+	  		str = str + stringifyJSON(obj[i]);
+	  	}
+
+	  	str = str + "]";
+
+  	} else {
+
+  		str = "[" + stringifyJSON(obj[0]) + "]"
+  	}
+  } else {
+  	
+  	if(typeof obj === "object"){
+  	str = str + obj;
+  	}
+  }
+
+  return str;
+  
 };
+
+function spliceStr(str, index, count, add) {
+  return str.slice(0, index) + (add || "") + str.slice(index + count);
+}
