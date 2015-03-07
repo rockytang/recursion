203dd7910d1de2d0f811bceb5f8cb0eebea95880
Rocky Tang <rocky.tang168@gmail.com>
2015-03-06 17:53:26 -0800

    getElementsByClassName completed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,17 +6,13 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
 
-  //the function I need to copy
-  var arr = document.getElementsByClassName(className);
-
-  var arr2 = Array.prototype.slice.apply(arr);
-  //I use this to compare to my answer I get later.
-
-
 var accept = function(node){
 	if(node.classList !== undefined) {
-		if(node.classList == className) {
-			output.push(node)
+		
+		for(var i = 0; i < node.classList.length; i++) {
+			if(node.classList[i] == className) {
+				output.push(node);
+			}
 		}
 	}
 };
@@ -36,56 +32,5 @@ var treeWalk = function(node){
   	treeWalk(document.childNodes[i]);
   }
 
-
-// This pulls the right nodes but I cheated a bit by looking at the testing suite
-// Having trouble using recursion with this
-/*
-	for(var i = 0; i < document.childNodes[1].childNodes.length; i++){
-		source = document.childNodes[1].childNodes[i];
-		accept(source);
-
-		var sLength = source.childNodes.length;
-
-		for(var j = 0; j < sLength; j++){
-			source2 = source.childNodes[j];
-			accept(source2);
-		}
-	} 
-*/
-
-debugger;
 	return output;
-
 };
-
-//A treewalker solution I found at http://ejohn.org/blog/getelementsbyclassname-speed-comparison/
-//as reference only
-
-  //document.getElementsByClass = function(needle) {
-
-  /*
-  function acceptNode(node) {
-    if (node.hasAttribute("class")) {
-      var c = " " + node.className + " ";
-       if (c.indexOf(className) != -1)
-         return NodeFilter.FILTER_ACCEPT;
-    } 
-    return NodeFilter.FILTER_SKIP;
-  }
-  
-
-  var treeWalker = document.createTreeWalker(document.documentElement,
-      NodeFilter.SHOW_ELEMENT, acceptNode, true);
-
-  var outArray = new Array();
-
-  if (treeWalker) {
-    var node = treeWalker.nextNode();
-    while (node) {
-      outArray.push(node);
-      node = treeWalker.nextNode();
-    }
-  }
-
-  return outArray;
-*/
